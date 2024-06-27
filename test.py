62ecc33053f5644b5c368c6bced25ed196d11ca6
Muhammad-Shah <msh87376@gmail.com>
2024-06-27 18:04:18 +0500

    rough

--- test.py
+++ test.py
@@ -1,53 +1,94 @@
-import streamlit as st
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-from streamlit_chat import message
-import requests
+# from langchain.document_loaders import DirectoryLoader
+# from langchain.text_splitter import CharacterTextSplitter
+# import os
+# import pinecone
+# from langchain.vectorstores import Pinecone
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.chains import RetrievalQA
+# from langchain.chat_models import ChatOpenAI
+# import streamlit as st
+# from dotenv import load_dotenv
 
-st.set_page_config(
-    page_title="Streamlit Chat - Demo",
-    page_icon=":robot:"
-)
-
-
-st.header("Streamlit Chat - Demo")
-
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+# load_dotenv()
 
 
-def generate_response(prompt):
-    """Generates a response using LLM."""
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
-                                 temperature=0.1,
-                                 max_tokens=100,
-                                 top_p=0.9,
-                                 google_api_key="AIzaSyAwFJZW0I1gA_954Wih96vDb3T0b-L9p84")
+# PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+# PINECONE_ENV = os.getenv('PINECONE_ENV')
+# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-    return llm.invoke(prompt)
+# os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 
 
-def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
-    return input_text
+# def doc_preprocessing():
+#     loader = DirectoryLoader(
+#         'data/',
+#         glob='**/*.pdf',     # only the PDFs
+#         show_progress=True
+#     )
+#     docs = loader.load()
+#     text_splitter = CharacterTextSplitter(
+#         chunk_size=1000,
+#         chunk_overlap=0
+#     )
+#     docs_split = text_splitter.split_documents(docs)
+#     return docs_split
 
 
-user_input = get_text()
+@st.cache_resource
+def embedding_db():
+    # we use the openAI embedding model
+    embeddings = OpenAIEmbeddings()
+    pinecone.init(
+        api_key=PINECONE_API_KEY,
+        environment=PINECONE_ENV
+    )
+    docs_split = doc_preprocessing()
+    doc_db = Pinecone.from_documents(
+        docs_split,
+        embeddings,
+        index_name='langchain-demo-indexes'
+    )
+    return doc_db
 
-if user_input:
-    output = generate_response(user_input).content
 
-    st.session_state.past.append(user_input)
+# llm = ChatOpenAI()
+# doc_db = embedding_db()
 
-    st.session_state.generated.append(output)
 
-if st.session_state['generated']:
+# def retrieval_answer(query):
+#     qa = RetrievalQA.from_chain_type(
+#         llm=llm,
+#         chain_type='stuff',
+#         retriever=doc_db.as_retriever(),
+#     )
+#     query = query
+#     result = qa.run(query)
+#     return result
 
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        # message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+# def main():
+#     st.title("Question and Answering App powered by LLM and Pinecone")
+
+#     text_input = st.text_input("Ask your query...")
+#     if st.button("Ask Query"):
+#         if len(text_input) > 0:
+#             st.info("Your Query: " + text_input)
+#             answer = retrieval_answer(text_input)
+#             st.success(answer)
+
+
+# if __name__ == "__main__":
+#     main()
+
+import os
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv('env')
+
+# Access your API key
+GOOGLE_API = os.getenv("GOOGLE_API")
+HF_API = os.getenv('HF_TOKEN')
+print(GOOGLE_API)
+print(HF_API)
