50e2585cc3bab0eac83827af8c8c62acaa366eac
Muhammad-Shah <msh87376@gmail.com>
2024-07-05 19:07:02 +0500

    ingestion

--- ingest.py
+++ ingest.py
@@ -1,33 +1,23 @@
-import sys
-__import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import jq
-from pprint import pprint
-from prompt import system_prompt
-from langchain_core.documents import Document
-from langchain_core.prompts import PromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import JSONLoader
-from langchain_community.vectorstores import Pinecone
-from langchain_core.runnables import RunnablePassthrough
-from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_core.output_parsers import StrOutputParser
-from langchain import hub
-from dotenv import load_dotenv
-import streamlit as st
 import os
+import streamlit as st
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.documents import Document
 from langchain_chroma import Chroma
+from prompt import system_prompt
+import jq
 
 # Load environment variables from .env file
 load_dotenv('.env')
 
 # Access your API key
-GOOGLE_API = os.getenv("GOOGLE_API")
-HF_API = os.getenv('HF_TOKEN')
-PINECONE_API_KEY = os.getenv('PINECONE_API')
+# GOOGLE_API = os.getenv("GOOGLE_API")
 
 
 def load_data():
@@ -35,34 +25,44 @@ def load_data():
     Load data from a file using the provided file path and JSON schema.
 
     Args:
-        file_path (str): The path to the file containing the data.
-        jq_schema (str): The JSON schema to be used for loading the data.
+        None
 
     Returns:
         list: A list of Document objects created from the loaded data.
+
+    Raises:
+        FileNotFoundError: If the file specified in `file_path` does not exist.
+        Exception: If an unexpected error occurs during the loading process.
     """
     try:
+        # Set the file path and JSON schema
         file_path = "data/FAQ.json"
         jq_schema = '.[]'
-        
+
         # Check if file exists
-        import os
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"The file {file_path} does not exist.")
-        
+
+        # Create a JSONLoader instance with the specified file path, JSON schema, and text content flag
         loader = JSONLoader(
             file_path=file_path,
             jq_schema=jq_schema,
             text_content=False
         )
-        
+
+        # Load the data using the JSONLoader instance
         data = loader.load()
+
+        # Create Document objects from the loaded data
         data = [Document(page_content=doc.page_content) for doc in data]
+
         return data
-    
+
     except FileNotFoundError as e:
+        # Print an error message if the file does not exist
         print(f"File error: {e}")
     except Exception as e:
+        # Print an error message for any unexpected errors
         print(f"An unexpected error occurred: {e}")
 
 
@@ -76,8 +76,13 @@ def create_embeddings(model_name):
 
     Returns:
         HuggingFaceEmbeddings: The cached instance of `HuggingFaceEmbeddings` with the specified `model_name`.
+
     """
+    # Initialize Hugging Face Embeddings with the specified model name
+    # This instance is cached for future use within the Streamlit session
     embeddings = HuggingFaceEmbeddings(model_name=model_name)
+
+    # Return the cached instance of Hugging Face Embeddings
     return embeddings
 
 # def create_embeddings(model_name, hf_api_key, texts):
@@ -94,24 +99,31 @@ def create_embeddings(model_name):
 @st.cache_resource
 def create_chroma_db(persist_directory, _embeddings):
     """
-    Creates a Chroma database using the given file path, JSON schema, persist directory, and embedding function.
+    Creates a Chroma database using the given persist directory and embedding function.
+
+    If a directory named 'vectorstore' exists in the current working directory, it loads the database from the directory.
+    Otherwise, it loads the data from a JSON file, creates the database from the documents, and persists it to the specified directory.
 
     Args:
-        file_path (str): The path to the file containing the data.
-        jq_schema (str): The JSON schema to be used for loading the data.
         persist_directory (str): The directory where the database will be persisted.
         _embeddings (HuggingFaceEmbeddings): The embedding function to use.
 
     Returns:
         Chroma: The created Chroma database.
     """
+
+    # Check if the 'vectorstore' directory exists
     if os.path.isdir(os.getcwd() + '/vectorstore'):
+        # Load the database from the directory
         db = Chroma(persist_directory=persist_directory,
                     embedding_function=_embeddings)
     else:
+        # Load the data from a JSON file
         data = load_data()
+        # Create the database from the documents
         db = Chroma.from_documents(
             data, _embeddings, persist_directory=persist_directory)
+
     return db
 
 
@@ -135,16 +147,17 @@ def create_pinecone_db(data, _embeddings):
         The repository used is DENSE.
     """
     # Initialize Pinecone client
-    pc = Pinecone(api_key=PINECONE_API_KEY)
+    pc = Pinecone(api_key=PINECONE_API_KEY)  # Initialize Pinecone client
 
     # Create a Pinecone index from documents
-    index_name = 'pinecone'
+    index_name = 'pinecone'  # Name of the index
     index = pc.create_index(
-        name=index_name,
-        service_type=ServiceType.SIMILARITY,
-        dimension=1536,  # Assuming embeddings is a numpy array
-        repository=Repository.DENSE,
-        data=docs_split
+        name=index_name,  # Name of the index
+        service_type=ServiceType.SIMILARITY,  # Service type for similarity search
+        # Dimension of the index (assuming embeddings is a numpy array)
+        dimension=1536,
+        repository=Repository.DENSE,  # Repository type
+        data=data  # Data to be indexed
     )
 
     return index
@@ -163,12 +176,19 @@ def create_llm(model, temperature, max_tokens, top_p, GOOGLE_API):
 
     Returns:
         ChatGoogleGenerativeAI: The created ChatGoogleGenerativeAI object.
+
+    Note:
+        The ChatGoogleGenerativeAI is a language model that can generate human-like text based on a prompt.
+        The parameters control the style and length of the generated text.
     """
-    llm = ChatGoogleGenerativeAI(model=model,
-                                 temperature=temperature,
-                                 max_tokens=max_tokens,
-                                 top_p=top_p,
-                                 google_api_key=GOOGLE_API)
+    # Create a ChatGoogleGenerativeAI object with the given parameters
+    llm = ChatGoogleGenerativeAI(
+        model=model,  # The name of the model to use
+        temperature=temperature,  # The temperature parameter for the model
+        max_tokens=max_tokens,  # The maximum number of tokens to generate
+        top_p=top_p,  # The top-p parameter for the model
+        google_api_key=GOOGLE_API  # The API key for the model
+    )
     return llm
 
 
@@ -181,10 +201,21 @@ def create_prompt(system_prompt):
 
     Returns:
         ChatPromptTemplate: The created chat prompt template.
+
+    Note:
+        The ChatPromptTemplate is a class that represents a chat prompt template.
+        It consists of a system prompt and a human prompt.
+        The system prompt is used to provide context and instructions to the model.
+        The human prompt is used to prompt the model to generate a response.
     """
+    # Create a chat prompt template with the given system prompt and human prompt
+    # The system prompt is used to provide context and instructions to the model
+    # The human prompt is used to prompt the model to generate a response
     prompt = ChatPromptTemplate.from_messages(
         [
+            # The system prompt is used to provide context and instructions to the model
             ("system", system_prompt),
+            # The human prompt is used to prompt the model to generate a response
             ("human", "{Question}"),
         ]
     )
@@ -252,7 +283,9 @@ def process_query(rag_chain, query, correction_prompt_template, llm):
         str: The processed query with any grammar and spelling errors corrected.
     """
     standalone_question = llm.invoke(correction_prompt_template).content
+    print(standalone_question)
     result = rag_chain.invoke(standalone_question)
+    print(result)
     return result
 
 
@@ -262,10 +295,7 @@ def rag_pipeline(retriever, model, temperature, max_tokens, top_p, GOOGLE_API, s
     Runs a pipeline to process a user query using a Retrieval-Augmented Generation (RAG) approach.
 
     Args:
-        file_path (str): The path to the file containing the data to be ingested.
-        jq_schema (str): The JSON query schema used to extract data from the file.
-        embeddings (Embeddings): The embeddings model used to encode the data.
-        persist_directory (str): The directory where the database will be persisted.
+        retriever (Retriever): The retriever object used to retrieve relevant data for the query.
         model (str): The name of the language model used for generation.
         temperature (float): The temperature parameter for the language model.
         max_tokens (int): The maximum number of tokens to generate for the language model.
@@ -277,9 +307,6 @@ def rag_pipeline(retriever, model, temperature, max_tokens, top_p, GOOGLE_API, s
     Returns:
         str: The processed query with any grammar and spelling errors corrected.
     """
-
-    # # Create the database
-    # db = create_pinecone_db(data, embeddings)
 
     # Create the LLM
     llm = create_llm(model, temperature, max_tokens, top_p, GOOGLE_API)
@@ -298,20 +325,3 @@ def rag_pipeline(retriever, model, temperature, max_tokens, top_p, GOOGLE_API, s
         rag_chain, query, correction_prompt_template, llm)
 
     return result
-
-
-# # Example usage for Testing
-# file_path = "data/FAQ.json"
-# jq_schema = ".[]"
-# model_name = "sentence-transformers/all-mpnet-base-v2"
-# persist_directory = "./vectorstore"
-# model = "gemini-1.5-flash"
-# temperature = 0.1
-# max_tokens = 512
-# top_p = 0.9
-# query = "How can I create an account?"
-
-# result = rag_pipeline(file_path, jq_schema, model_name,
-#                       persist_directory, model, temperature,
-#                       max_tokens, top_p, GOOGLE_API, system_prompt, query)
-# pprint(result)
